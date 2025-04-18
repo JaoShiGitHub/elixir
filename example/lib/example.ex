2527a7431f07184a4a3af7ef6091eb5c053a1af3
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-18 20:05:37 +0700

    Atom

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,15 +1,19 @@
 defmodule Example do
   use Application
 
-  @x 5
-
   def start(_type, _args) do
     Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
   def main do
-    # x = 10
-    IO.puts(@x)
+    name = "Caleb"
+    status = Enum.random([:gold, :silver, :bronze, :"not a member"])
+
+    case status do
+      :gold -> IO.puts("Welcome to the fancy lounge, #{name}")
+      :"not a member" -> IO.puts("Get Subscribed")
+      _ -> IO.puts("Get out bruh")
+    end
   end
 end
